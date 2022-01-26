6953b2fe13b1f8d98bbb10d1fcb253564f349be7
AhmadMBahattab <xpazzax11@gmail.com>
2022-01-26 14:31:20 +0300

    complete orders screen .

--- app/screens/OrdersScreen.js
+++ app/screens/OrdersScreen.js
@@ -1,21 +1,56 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import BackToMainHeader from "../components/reusable/BackToMainHeader";
+import { AntDesign } from "@expo/vector-icons";
 
 const OrderScreen = () => {
   return (
     <>
       <BackToMainHeader title={"طلباتي"} />
-      <View style={styles.container}>
-        <Text>Orders</Text>
-      </View>
+      <ScrollView>
+        <TouchableOpacity>
+          <View style={styles.container}>
+            <View>
+              <Text style={styles.orderInfo}>هرفي - Herfy</Text>
+              <Text style={styles.orderInfo}>الطلب - #995434</Text>
+              <Text style={{ color: "gray" }}>04-02-2022 10:10 PM</Text>
+            </View>
+            <Text></Text>
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={{ color: "red", margin: 10 }}>تم التوصيل</Text>
+                <AntDesign name="left" size={6} color="black" />
+              </View>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
     </>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomColor: "lightgray",
+    borderBottomWidth: 1,
+  },
+  orderInfo: {
+    color: "gray",
+    fontSize: 17,
+    margin: 3,
   },
 });
 
